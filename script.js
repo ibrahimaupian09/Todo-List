af3d4c8ae929f9137eb70f3adcda1b85d1568fd0
ibrahimaupian09 <ibrahimaupian09@gmail.com>
2024-09-26 21:55:58 +0500

    Added remove button

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function addTask() {
 
     var li = document.createElement('li');
             li.textContent = taskInput.value;
+
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn';
+    removeButton.onclick = function () {
+    taskList.removeChild(li);
+    };
