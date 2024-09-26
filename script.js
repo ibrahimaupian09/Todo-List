234a1f801bd6503491a1b56d8e674d635ff6785f
ibrahimaupian09 <ibrahimaupian09@gmail.com>
2024-09-26 21:45:15 +0500

    add styling, create list

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function addTask() {
     var taskInput = document.getElementById('taskInput');
     var taskList = document.getElementById('taskList');
+
+    var li = document.createElement('li');
+            li.textContent = taskInput.value;
